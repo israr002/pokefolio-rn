79ac73d223ccaea646f82697234dff3cab84dce6
israr002 <israr002khan@gmail.com>
2025-04-07 23:15:46 +0530

    chore: project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          api: './src/api',
+          assets: './src/assets',
+          components: './src/components',
+          constants: './src/constants',
+          hooks: './src/hooks',
+          navigation: './src/navigation',
+          screens: './src/screens',
+          styles: './src/styles',
+          tests: './tests',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
